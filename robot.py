f0d6ca5f9dea9d5cfdc6c6979ab1e087c25f2757
liuchang0812@gmail.com <liuchang0812@gmail.com@d5292801-9e47-482c-a66c-fb6f1e3eb4d8>
2013-04-13 17:17:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/robot.py
    git-svn-id: https://svn.sinaapp.com/wechatforwuluo/2@342 d5292801-9e47-482c-a66c-fb6f1e3eb4d8

--- robot.py
+++ robot.py
@@ -24,14 +24,16 @@ def parsetext(message):
     print message.content
     
     msg = str(message.content)
+   
+    user = str(message.source)
     
-    print msg 
+    print msg , user
     
     if msg == u"最近比赛" or msg==u"比赛" or msg ==u"contests" :
         if datetime.now().day != lastconteststime.day:
             cont.update()
 
-        reply = ArticlesReply()
+        reply = ArticlesReply(message=message)
         reply.add_article(
             Article(
             title = "发现最近有不少比赛哟！快去虐菜吧！！" ,
@@ -40,12 +42,14 @@ def parsetext(message):
             description= '点击下面看详细   ps:比赛信息由acmicpc提供'
             )
         )
+        '''
         for i in range(min(7,len(cont))):
             reply.add_article(Article(
                 title= cont[i]['oj'] + " : " + cont[i]['name'] ,
                 url=cont[i]['link'] ,
                 description= cont[i]['start']
             ))
+        '''
         return reply
     return helpstr
 
