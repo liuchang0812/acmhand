06e45dd79507f69da1a13aa822c6796742e19a45
liuchang0812@gmail.com <liuchang0812@gmail.com@d5292801-9e47-482c-a66c-fb6f1e3eb4d8>
2013-04-13 17:21:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/robot.py
    git-svn-id: https://svn.sinaapp.com/wechatforwuluo/2@347 d5292801-9e47-482c-a66c-fb6f1e3eb4d8

--- robot.py
+++ robot.py
@@ -36,19 +36,17 @@ def parsetext(message):
         reply = ArticlesReply(message=message)
         reply.add_article(
             Article(
-            title = "发现最近有不少比赛哟！快去虐菜吧！！" ,
+            title = "发现最近有不少比赛哟！快去虐菜吧！！点击下面看详细   ps:比赛信息由acmicpc提供" ,
             img = "https://raw.github.com/Liuchang0812/acmhand/master/acmhand/img/acmlogo.jpg" ,
             url = " http://acmicpc.info",
-            description= '点击下面看详细   ps:比赛信息由acmicpc提供'
+            description= ''
             )
         )
-        
         for i in range(min(7,len(cont))):
             reply.add_article(Article(
-                title= cont[i]['oj'] + " : " + cont[i]['name'] ,
+                title= cont[i]['oj'] +'\b' + cont[i]['start_time'] + " \n " + cont[i]['name'] ,
                 url=cont[i]['link'] ,
-                description= cont[i]['start_time'] ,
-                img = ''
+                description= ''
             ))
         
         return reply
