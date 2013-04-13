8cd5ab29d0c41997ebf708aeb4932ecb1f178ffa
liuchang0812@gmail.com <liuchang0812@gmail.com@d5292801-9e47-482c-a66c-fb6f1e3eb4d8>
2013-04-13 16:23:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/rebot.py
    git-svn-id: https://svn.sinaapp.com/wechatforwuluo/2@319 d5292801-9e47-482c-a66c-fb6f1e3eb4d8

--- rebot.py
+++ rebot.py
@@ -1,11 +1,6 @@
 #-*- encoding:utf-8 -*-
 __author__ = 'liuchang'
 
-import os
-import sys
-
-root = os.path.dirname(__file__)
-sys.path.insert(0 , os.path.join(root,'site-packages'))
 
 import werobot
 robot = werobot.WeRoBot(token='helloacmer')
