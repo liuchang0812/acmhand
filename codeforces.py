d026180f1681d17c8ea452ab110a702a5826d334
liuchang0812@gmail.com <liuchang0812@gmail.com@d5292801-9e47-482c-a66c-fb6f1e3eb4d8>
2013-04-27 07:29:50 +0000

    git-svn-id: https://svn.sinaapp.com/wechatforwuluo/2@393 d5292801-9e47-482c-a66c-fb6f1e3eb4d8

---
+++ codeforces.py
@@ -0,0 +1,40 @@
+# -*- encoding:utf-8 -*-
+from urllib import urlopen
+import re
+import json
+class codeforces(object):
+    def __init__(self):
+        self.url = 'http://codeforces.com/profile/%s'
+
+    def getrating(self , id ):
+        profileurl = self.url % id
+        print profileurl
+        r = urlopen(profileurl)
+        if r.geturl() == profileurl :
+            rep = r.read()
+            #print rep
+            st = rep.index("data.push(")
+            st = st + 10
+            ed = rep.index(");" , st)
+            s = rep[st:ed]
+            #print s
+            js = json.loads(s)
+            print js[len(js) - 1]
+            contest_name = js[len(js) - 1][3]
+            rating = js[len(js) - 1][1]
+            rank = js[len(js) - 1 ][6]
+            change = js[len(js) -1 ][5]
+            print contest_name , rating , rank , change
+            if  int(change) > 0 :
+                return u"%s 在最近的 %s 比赛中，排名 %s ，rating 变为 %s , 涨了 %s 点。Orz神牛啊~"%( id , contest_name , rank , rating , change)
+            else:
+                return u"%s 在最近的 %s 比赛中，排名 %s , rating 变为 %s , 跌了 %s 点。继续加油吧~"%( id , contest_name , rank , rating , change)
+        return " 好想没有这个ID，请重新输入 "
+
+if __name__ == "__main__":
+
+    cd = codeforces()
+    print   cd.getrating('chang.mei')
+            
+            
+        
